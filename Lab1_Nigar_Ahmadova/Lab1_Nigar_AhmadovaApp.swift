282214148d9f8bde2a19da3f8b5906333c7b5cb5
Nigar Ahmadova <nigar.ahmadova@georgebrown.ca>
2025-02-12 10:54:00 -0500

    Initial commit

---
+++ Lab1_Nigar_Ahmadova/Lab1_Nigar_AhmadovaApp.swift
@@ -0,0 +1,17 @@
+//
+//  Lab1_Nigar_AhmadovaApp.swift
+//  Lab1_Nigar_Ahmadova
+//
+//  Created by Nigar Ahmadova on 2025-02-12.
+//
+
+import SwiftUI
+
+@main
+struct Lab1_Nigar_AhmadovaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Lab1_Nigar_AhmadovaTests/Lab1_Nigar_AhmadovaTests.swift
@@ -0,0 +1,17 @@
+//
+//  Lab1_Nigar_AhmadovaTests.swift
+//  Lab1_Nigar_AhmadovaTests
+//
+//  Created by Nigar Ahmadova on 2025-02-12.
+//
+
+import Testing
+@testable import Lab1_Nigar_Ahmadova
+
+struct Lab1_Nigar_AhmadovaTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
