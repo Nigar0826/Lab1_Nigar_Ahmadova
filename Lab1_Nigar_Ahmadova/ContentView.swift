680f5fe99a96840585a4d2f2c702d118d4e8f854
Nigar Ahmadova <nigar.ahmadova@georgebrown.ca>
2025-02-12 11:29:46 -0500

    Added random number generation on app start

--- Lab1_Nigar_Ahmadova/ContentView.swift
+++ Lab1_Nigar_Ahmadova/ContentView.swift
@@ -8,15 +8,38 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State private var number = Int.random(in: 1...100) // Generate a random number
+
     var body: some View {
         VStack {
             Text("Is this number prime?")
                 .font(.title)
                 .padding()
             
-            Text("0") // Placeholder for random number
+            Text("\(number)") // Display the random number
                 .font(.system(size: 60, weight: .bold))
                 .padding()
+            
+            HStack {
+                Button("Prime") {
+                    print("Prime button tapped")
+                }
+                .font(.title)
+                .frame(width: 120, height: 50)
+                .background(Color.blue)
+                .foregroundColor(.white)
+                .cornerRadius(10)
+                
+                Button("Not Prime") {
+                    print("Not Prime button tapped")
+                }
+                .font(.title)
+                .frame(width: 120, height: 50)
+                .background(Color.red)
+                .foregroundColor(.white)
+                .cornerRadius(10)
+            }
+            .padding()
         }
     }
 }
